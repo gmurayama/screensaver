d0c05fc6a9ffc2725c129542648081aedaa46008
Gustavo Murayama <gmurayama@outlook.com>
2019-10-31 15:04:55 -0300

    Added random color to points in screen

--- fshader.glsl
+++ fshader.glsl
@@ -1,8 +1,9 @@
 #version 410
 
+in vec4 v2fcolor;
 out vec4 myfragcolor;
 
 void main()
 {
-    myfragcolor = vec4(1,1,1,1);
+    myfragcolor = v2fcolor;
 }
--- vshader.glsl
+++ vshader.glsl
@@ -5,8 +5,21 @@ out vec4 v2fcolor;
 
 uniform vec4 translation;
 
+float random (vec2 st) {
+    return fract(sin(dot(st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
+}
+
 void main()
 {
     float scaling = (translation.z - 1) / 10;
     gl_Position = (vPosition + translation) * vec4(scaling, scaling, scaling, 1);
+
+    float red = abs(translation.x) * random(translation.xz);
+    float green = abs(translation.y) * random(translation.yz);
+    float blue = abs(translation.z) * random(translation.xy);
+
+    vec4 color = vec4(red, green, blue, 1);
+    v2fcolor = color;
 }
